9d4d0586b15ab2925733f1b739a21ac10541476e
Gramof0n <39032903+Gramof0n@users.noreply.github.com>
2018-05-07 17:48:20 +0200

    Add files via upload

---
+++ TheNumbersMason/src/GUI.java
@@ -0,0 +1,53 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel panel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 851, 561);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getPanel());
+		Numbers numbers = new Numbers(panel);
+		numbers.start();
+	}
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setBackground(Color.BLACK);
+			panel.setBounds(0, 0, 845, 533);
+		}
+		return panel;
+	}
+}
---
+++ TheNumbersMason/src/Numbers.java
@@ -0,0 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.util.Random;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class Numbers extends Thread {
+
+	private JLabel label;
+	private JPanel panel;
+
+	public Numbers(JPanel panel) {
+		super();
+		this.panel = panel;
+	}
+
+	@Override
+	public void run() {
+		Random r = new Random();
+
+		JLabel[] labels = new JLabel[10000];
+		String[] numbers = { "1", "2", "3", "4", "5", "6"," 7", "8", "9", "0" };
+		
+		int x = 0;
+		int y = 0;
+		JLabel l = null;
+		for (int i = 0; i < labels.length; i++) {
+			 x = r.nextInt(panel.getBounds().width);
+			 y = r.nextInt(panel.getBounds().height);
+			
+			l = new JLabel();
+			l.setBounds(x, y , 10, 10);
+			l.setFont(new Font("Tahoma", Font.PLAIN, 10));
+			l.setForeground(Color.GREEN);
+			l.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+			labels[i] = l;
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			panel.add(l);
+			panel.repaint();
+			
+		}
+		
+		
+		for (int i = 0; i < labels.length; i++) {
+			l.setText(numbers[r.nextInt(10)]);
+			panel.repaint();
+		}
+		
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(labels.length);
+
+	}
+}
